3822f779588a4a63bb467502ed9cc013914affe7
Vamshi-78 <bandarivamshi9949@gmail.com>
2026-07-29 21:44:54 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n=temperatures.length;
+        int[] res=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&&temperatures[i]>temperatures[st.peek()]){
+            int index=st.pop();
+            res[index]=i-index;
+            }
+            st.push(i);
+        }
+        return res;
+    }
+}
